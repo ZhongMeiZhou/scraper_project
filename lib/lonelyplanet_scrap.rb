82b7ad6fa954fe725d538829c6f3ac6dfa0b061e
eduardo-salazar <esalazar922@gmail.com>
2015-10-07 23:23:35 +0800

    Added main structure of module lonelyplanet

--- lib/lonelyplanet_scrap.rb
+++ lib/lonelyplanet_scrap.rb
@@ -1,3 +1,11 @@
+require 'oga'
+require 'open-uri'
+require 'date'
+
+# This module include all the classes that Get data from LonelyPlanet wesite
 module LonelyplanetScrap
-  # Your code goes here...
+  # This class gets the news from the website http://www.lonelyplanet.com/news/
+  class LonelyPlaneNews
+
+  end
 end
