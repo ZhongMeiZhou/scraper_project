5ba200857b81e316190f9982bffa4d22a15c3e98
soa <soa@localhost.localdomain>
2015-10-09 01:17:11 +0800

    Modifications to spec

--- spec/lonelyplanet_spec.rb
+++ spec/lonelyplanet_spec.rb
@@ -1,33 +1,23 @@
 require 'minitest/autorun'
-<<<<<<< HEAD
-
-class ScraperTest
-	include 
-
-
-end
-
-=======
 require 'minitest/rg'
 require 'yaml'
 require 'json'
-require_relative './lib/lonelyplanet_scrap.rb'
+require './lib/lonelyplanet_scrap'
 
+# Possible tests to add:
+# check if data equal to what is in tours.yml (can convert to yaml and compare)
 
-tours_from_file = YAML.load(File.read('tours.yml'))
+tours_from_file = YAML.load(File.read('./spec/tours.yml'))
 
 describe "Comparing actual data and detecting changes in tours" do
 
 	before do
-		data = LonelyPlanetScrape::LonelyPlanetTours.new.get_tours
+		data = LonelyPlanetScrape::LonelyPlanetTours.new.tours
 		@tours_found = JSON.parse(data)
-
-
 	end
 
-	it 'has the same number of tours' do
+	it 'has the right number of tours' do
 		@tours_found.size.must_equal tours_from_file.size
 	end
 
 end
->>>>>>> df1dddbaaead0e2966a228bfc2c457b80664ae14
