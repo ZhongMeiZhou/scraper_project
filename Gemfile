61265087eb11d2fe0356aca950859a0871389061
bjsalgado <bayardo_salgado@yahoo.com>
2015-10-08 18:35:36 +0800

    Gemfile modified with more gems; first test of spec

--- Gemfile
+++ Gemfile
@@ -2,3 +2,6 @@ source 'https://rubygems.org'
 
 # Gems required to run lonelyplanet_spec
 gem 'oga'
+gem 'json'
+gem 'minitest'
+
--- spec/lonelyplanet_spec.rb
+++ spec/lonelyplanet_spec.rb
@@ -1,1 +1,23 @@
-specs
+require 'minitest/autorun'
+require 'minitest/rg'
+require 'yaml'
+require 'json'
+require_relative './lib/lonelyplanet_scrap.rb'
+
+
+tours_from_file = YAML.load(File.read('tours.yml'))
+
+describe "Comparing actual data and detecting changes in tours" do
+
+	before do
+		data = LonelyPlanetScrape::LonelyPlanetTours.new.get_tours
+		@tours_found = JSON.parse(data)
+
+
+	end
+
+	it 'has the same number of tours' do
+		@tours_found.size.must_equal tours_from_file.size
+	end
+
+end
