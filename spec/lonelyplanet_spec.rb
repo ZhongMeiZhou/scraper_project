fe92562df9afcddfb89215e19989ecec1c7cca22
nweat <nikkiweat@gmail.com>
2015-10-14 22:46:13 +0800

    Added VCR spec improvements

--- spec/lonelyplanet_spec.rb
+++ spec/lonelyplanet_spec.rb
@@ -1,47 +1,42 @@
 require 'minitest/autorun'
-#require 'minitest/rg'
+# require 'minitest/rg'
 require 'yaml'
 require 'json'
 require 'vcr'
 require 'webmock/minitest'
 require './lib/lonelyplanet_scrap'
+require './spec/support/vcr_setup'
 
 tours_from_file = YAML.load(File.read('./spec/tours.yml'))
 
-
-VCR.configure do |config|
-  config.cassette_library_dir = 'spec/fixtures/vcr_cassettes'
-  config.hook_into :webmock
-end
-
-VCR.use_cassette('lonelyplanettours') do
+VCR.use_cassette('taiwan_tours') do
   data = LonelyPlanetScrape::LonelyPlanetTours.new.tours
   tours_found = JSON.parse(data)
 
-describe 'Compare returned results against actual data' do
+  describe 'Compare returned results against actual data' do
 
-  it 'check if the number of taiwan tours has changed' do
-    tours_found.size.must_equal tours_from_file.size
-  end
-
-  it 'check for changes in structure of results' do
-    tours_found[0].key?('price').must_equal true
-    tours_found[0].key?('title').must_equal true
-    tours_found[0].key?('content').must_equal true
-  end
-
-  0.upto(tours_from_file.length - 1) do |index|
-    it 'check for price changes' do
-      tours_from_file[index]['price'].must_equal tours_found[index]['price']
+    it 'check if the number of taiwan tours has changed' do
+      tours_found.size.must_equal tours_from_file.size
     end
 
-    it 'check for title changes' do
-      tours_from_file[index]['title'].must_equal tours_found[index]['title']
+    it 'check for changes in structure of results' do
+      tours_found[0].key?('price').must_equal true
+      tours_found[0].key?('title').must_equal true
+      tours_found[0].key?('content').must_equal true
     end
 
-    it 'check for description changes' do
-      tours_from_file[index]['content'].must_equal tours_found[index]['content']
+    0.upto(tours_from_file.length - 1) do |index|
+      it 'check for price changes' do
+        tours_from_file[index]['price'].must_equal tours_found[index]['price']
+      end
+
+      it 'check for title changes' do
+        tours_from_file[index]['title'].must_equal tours_found[index]['title']
+      end
+
+      it 'check for description changes' do
+        tours_from_file[index]['content'].must_equal tours_found[index]['content']
+      end
     end
   end
-end
 end
