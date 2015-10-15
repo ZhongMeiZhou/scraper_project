f2e1022f26919978a7ca467d02c0516674ec3d57
nweat <nikkiweat@gmail.com>
2015-10-15 09:40:52 +0800

    Few revisions to specs

--- spec/lonelyplanet_spec.rb
+++ spec/lonelyplanet_spec.rb
@@ -1,5 +1,7 @@
 require 'minitest/autorun'
 # require 'minitest/rg'
+require 'oga'
+require 'open-uri'
 require 'yaml'
 require 'json'
 require 'vcr'
@@ -10,33 +12,27 @@ require './spec/support/vcr_setup'
 tours_from_file = YAML.load(File.read('./spec/tours.yml'))
 
 VCR.use_cassette('taiwan_tours') do
-  data = LonelyPlanetScrape::LonelyPlanetTours.new.tours
-  tours_found = JSON.parse(data)
+  obj = LonelyPlanetScrape::LonelyPlanetTours.new
+  tours_found = JSON.parse(obj.tours) if !obj.tours.nil?
 
-  describe 'Compare returned results against actual data' do
+  describe 'Check for difference between returned results and actual data and possibly HTML structure changes' do
 
     it 'check if the number of taiwan tours has changed' do
       tours_found.size.must_equal tours_from_file.size
     end
 
-    it 'check for changes in structure of results' do
-      tours_found[0].key?('price').must_equal true
-      tours_found[0].key?('title').must_equal true
-      tours_found[0].key?('content').must_equal true
-    end
-
     0.upto(tours_from_file.length - 1) do |index|
-      it 'check for price changes' do
-        tours_from_file[index]['price'].must_equal tours_found[index]['price']
-      end
-
-      it 'check for title changes' do
-        tours_from_file[index]['title'].must_equal tours_found[index]['title']
-      end
-
-      it 'check for description changes' do
-        tours_from_file[index]['content'].must_equal tours_found[index]['content']
-      end
+    it 'check for price changes' do
+      tours_from_file[index]['price'].must_equal tours_found[index]['price']
     end
+
+    it 'check for title changes' do
+      tours_from_file[index]['title'].must_equal tours_found[index]['title']
+    end
+
+    it 'check for description changes' do
+      tours_from_file[index]['content'].must_equal tours_found[index]['content']
+    end
+   end  
   end
 end
